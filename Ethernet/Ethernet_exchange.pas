7cd7ea7cda8b5e67c96b96855686a451d3ea5a4a
SlavaX600 <SlavaX600@b9fb19ef-7c42-8a74-c7e1-06ee740589af>
2011-12-05 18:21:08 +0000

    [+] Реализован обмен средствами WinSock

--- Ethernet/Ethernet_exchange.pas
+++ Ethernet/Ethernet_exchange.pas
@@ -3,9 +3,9 @@ unit Ethernet_exchange;
 interface
 
 uses
-  //Для использования модуля ScktComp нужно добавить компонент
-  //\\Embarcadero\RAD Studio\7.0\bin\dclsockets70.bpl
-  ScktComp;
+
+  Windows,
+  winsock;
 
 type
 
@@ -16,58 +16,29 @@ type
 
   TClientSocketWorker = class
     private
-      m_Client_Socket:TClientSocket;
+      m_Socket: TSocket;
+      m_ServerAddress:TSockAddr;
+      m_WSAData:TWSAData;
+
       m_Active:Boolean;
       m_State: Boolean;
 
     public
 
-      constructor Create(IPAdress:string; Port:Integer);
-
-      Destructor  Destroy;
-      //события
-      ///возникает при установлении соединения. Т.е. сразу после установления
-      ///соединения
-      procedure OnConnect(Sender: TObject;  Socket: TCustomWinSocket);
-
-      ///возникает при установлении соединения. Отличие от OnConnect в том,
-      ///что соединение еще не установлено. Обычно такие промежуточные события
-      ///используются для обновления статуса;
-      procedure OnConnecting(Sender: TObject; Socket: TCustomWinSocket);
-
-      ///Возникает при закрытии сокета.
-      ///Причем, закрытия как из программы, так и со строноны удаленного
-      ///компьютера (либо из-за сбоя);
-      procedure OnDisconnect(Sender: TObject; Socket: TCustomWinSocket);
-
-      ///Возникает при ошибке в работе сокета.
-      ///Следует отметить, что это событие не поможет отловить ошибку в момент
-      ///открытия сокета (Open).
-      procedure OnError(Sender: TObject; Socket: TCustomWinSocket;
-                        ErrorEvent: TErrorEvent; var ErrorCode: Integer);
-
-      ///Возникает, когда удаленный компьютер послал Нам какие-либо данные.
-      // При возникновении этого события возможна обработка данных;
-      procedure OnRead(Sender: TObject; Socket: TCustomWinSocket);
-
-      ///возникает, когда Нам разрешена запись данных в сокет
-      procedure OnWrite(Sender: TObject; Socket: TCustomWinSocket);
+      constructor Create();
+      destructor  Destroy;
 
       function SetServerParametrs(IPAdress:string; Port:Integer):Integer;
-
-      procedure Open();
-      procedure Close();
+      function Open():Integer;
+      function Close():Integer;
 
       function WriteBlock(pBlock:Pointer; BlockSize:Word):Integer;
       function ReadBlock(pBlock:Pointer; var Count:Word; MaxBlockSize:Word):Integer;
       function IsActive():Boolean;
 
-
-
   end;
 var
-  m_ClientSocket:TClientSocket;
-
+  SocketWorker:TClientSocketWorker;
 const
   RET_OK = 0;
   RET_ERR = $FF;
@@ -78,32 +49,14 @@ const
 //-----------------------------------------------------------------------------
 implementation
 
-constructor TClientSocketWorker.Create(IPAdress:string; Port:Integer);
+constructor TClientSocketWorker.Create();
 begin
-  try
 
-    if (Port >= TCP_PORT_MIN_VALUE) Or (Port < TCP_PORT_MAX_VALUE) then
-    begin
+    WSAStartUp(MakeWord(2,0),m_WSAData);
+    m_ServerAddress.sin_family := AF_INET;
+    m_Socket := INVALID_SOCKET;
 
-      m_Client_Socket.OnConnect := OnConnect;
-      m_Client_Socket.OnConnecting := OnConnecting;
-      m_Client_Socket.OnDisconnect := OnDisconnect;
-      m_Client_Socket.OnRead := OnRead;
-      m_Client_Socket.OnWrite := OnWrite;
-      m_Client_Socket.OnError := OnError;
-
-      m_ClientSocket.Address := IPAdress;
-      m_ClientSocket.Port := Port;
-      m_State := True;
-
-    end
-    else
-       m_State := True;
-
-  except
     m_State := False;
-  end;
-
 
 end;
 
@@ -111,7 +64,14 @@ end;
 Destructor  TClientSocketWorker.Destroy;
 Begin
   try
-      m_Client_Socket.Destroy;
+
+    if m_Socket <> INVALID_SOCKET then
+    begin
+      closesocket(m_Socket);
+      m_Socket := INVALID_SOCKET;
+    end;
+
+    WSACleanup();
 
   finally
       m_Active := False;
@@ -120,102 +80,93 @@ Begin
 End;
 
 //-------------------------------------------------
-//события
-///возникает при установлении соединения. Т.е. сразу после установления
-///соединения
-procedure TClientSocketWorker.OnConnect(Sender: TObject;  Socket: TCustomWinSocket);
-begin
 
-end;
 
-//-------------------------------------------------
-///возникает при установлении соединения. Отличие от OnConnect в том,
-///что соединение еще не установлено. Обычно такие промежуточные события
-///используются для обновления статуса;
-procedure TClientSocketWorker.OnConnecting(Sender: TObject; Socket: TCustomWinSocket);
+function TClientSocketWorker.SetServerParametrs(IPAdress:string;
+  Port:Integer):Integer;
+  var
+  InAddr:TInAddr;
 Begin
+
+   InAddr.S_addr := inet_addr(PAnsiChar(IPAdress));
+   m_ServerAddress.sin_addr := InAddr;
+   m_ServerAddress.sin_port := htons(Port);
 
 End;
 
-///Возникает при закрытии сокета.
-///Причем, закрытия как из программы, так и со строноны удаленного
-///компьютера (либо из-за сбоя);
-procedure TClientSocketWorker.OnDisconnect(Sender: TObject; Socket: TCustomWinSocket);
+function TClientSocketWorker.Open():Integer;
 Begin
+    Result := RET_ERR;
 
+    if m_Socket <> INVALID_SOCKET then
+      begin
+       //усли переменная содержит адрес сокета, то перед открытием нового,
+       //старый нужно попытаться закрыть
+         closesocket(m_Socket);
+         m_Socket := INVALID_SOCKET
+      end;
+
+    m_Socket:=socket(AF_INET,SOCK_STREAM,0);
+
+    if m_Socket = INVALID_SOCKET then
+    begin
+      Result := RET_ERR;
+    end else
+    begin
+      if connect(m_Socket, m_ServerAddress, sizeof(m_ServerAddress)) = 0 then
+        Result := RET_OK;
+    end;
 End;
 
-///Возникает при ошибке в работе сокета.
-///Следует отметить, что это событие не поможет отловить ошибку в момент
-///открытия сокета (Open).
-procedure TClientSocketWorker.OnError(Sender: TObject; Socket: TCustomWinSocket;
-                  ErrorEvent: TErrorEvent; var ErrorCode: Integer);
+ function TClientSocketWorker.Close():Integer;
 Begin
 
-End;
+   Result := RET_ERR;
 
-///Возникает, когда удаленный компьютер послал Нам какие-либо данные.
-// При возникновении этого события возможна обработка данных;
-procedure TClientSocketWorker.OnRead(Sender: TObject; Socket: TCustomWinSocket);
-Begin
-
-End;
-
-///возникает, когда нам разрешена запись данных в сокет
-procedure TClientSocketWorker.OnWrite(Sender: TObject; Socket: TCustomWinSocket);
-Begin
-
-End;
-
-function TClientSocketWorker.SetServerParametrs(IPAdress:string; Port:Integer):Integer;
-Begin
-
-End;
-
-procedure TClientSocketWorker.Open();
-Begin
-
-End;
-
-procedure TClientSocketWorker.Close();
-Begin
+   if m_Socket <> INVALID_SOCKET then
+   begin
+      if closesocket(m_Socket) <> SOCKET_ERROR then
+      begin
+        m_Socket := INVALID_SOCKET;
+        Result := RET_OK;
+      end;
+   end;
 
 End;
 
 function TClientSocketWorker.WriteBlock(pBlock:Pointer; BlockSize:Word):Integer;
 Begin
 
+   Result := RET_ERR;
+
+   if m_Socket <> INVALID_SOCKET then
+   begin
+     if send(m_Socket, pBlock, BlockSize ,0) <> SOCKET_ERROR then
+        Result := RET_OK;
+   end;
+
 End;
 
-function TClientSocketWorker.ReadBlock(pBlock:Pointer; var Count:Word; MaxBlockSize:Word):Integer;
+function TClientSocketWorker.ReadBlock(pBlock:Pointer; var Count:Word;
+  MaxBlockSize:Word):Integer;
 Begin
+   Result := RET_ERR;
 
+   if m_Socket <> INVALID_SOCKET then
+   begin
+     Count := Recv(m_Socket, pBlock, MaxBlockSize,0);
+   end;
 End;
 
 function TClientSocketWorker.IsActive():Boolean;
 Begin
+   Result := False;
+
+   if m_Socket <> INVALID_SOCKET then
+   begin
+    Result := True;
+   end;
 
 End;
-
-
-//function CreateSocket(IPAdress:string; Port:Integer):Integer;
-//begin
-//  Result:= RET_OK;
-//  if (Port < TCP_PORT_MIN_VALUE) Or (Port > TCP_PORT_MAX_VALUE) then
-//    Result := RET_ERR
-//  else
-//  begin
-//  if m_ClientSocket.Active then //Если соединение уже установлено
-//    m_ClientSocket.Close;       //то оно закрывается
-//
-//    m_ClientSocket.Address := IPAdress;
-//    m_ClientSocket.Port := Port;
-//
-//    m_ClientSocket.Open;
-//
-//
-//  end;
-//
-//end;
 
 end.
